cc5860ad37df9b4fee958493182fdd2ffbbaa19d
Nathaly Oliveira <73591609+nathalyoliveira@users.noreply.github.com>
2021-08-31 10:52:25 -0300

    Aula 10

---
+++ Aula10/Aula10/Aula10/Jacare.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula10
+{
+    class Jacare : Reptil
+    {
+        public Jacare()
+        {
+            Console.Write("-> Jacaré\n");
+        }
+    }
+}
---
+++ Aula10/Aula10/Aula10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Aula10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cachorro c1 = new Cachorro();
+            Gato g1 = new Gato();
+            Jacare j1 = new Jacare();
+            Sapo s1 = new Sapo();
+        }
+    }
+}
